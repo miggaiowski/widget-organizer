7e59fb81b78a846f67848a03f5d73d08a3b1bdeb
Miguel Gaiowski <miggaiowski@gmail.com>
2012-08-31 16:39:04 -0300

    Spreads all Weather widgets vertically and updates de plist data

--- organizer.py
+++ organizer.py
@@ -6,6 +6,8 @@ import sys
 import pprint
 from plistparser import parser
 
+MY_PATH_TO_PLIST="~/Library/Preferences/com.apple.dashboard.plist"
+
 def plistToString(pathToPList):
     os.system("""/usr/libexec/PlistBuddy -c "Print" """ + pathToPList + " > temp.plist")
     with open("temp.plist", 'r') as f:
@@ -14,12 +16,41 @@ def plistToString(pathToPList):
     os.remove("temp.plist")
     return plist_lines
 
+def setIndices(plist):
+    for i, p in enumerate(plist['layer-gadgets']):
+        p['_pos'] = i
+
+def align(weathers):
+    delta = 165
+    for i, w in enumerate(weathers):
+        w['pos-y'] = i * delta
+
+def getCommands(weathers, pathToPList):
+    cmds = []
+    command = """/usr/libexec/PlistBuddy -c "Set %s" """ + pathToPList 
+    for w in weathers:
+        cmds.append(command % ('layer-gadgets:' + str(w['_pos']) + ":pos-y " + str(w['pos-y'])))
+        cmds.append(command % ('layer-gadgets:' + str(w['_pos']) + ":pos-x 20"))
+        cmds.append(command % ('layer-gadgets:' + str(w['_pos']) + ":percent-offset-x 0"))
+        cmds.append(command % ('layer-gadgets:' + str(w['_pos']) + ":percent-offset-y 0"))
+        cmds.append(command % ('layer-gadgets:' + str(w['_pos']) + ":percent-y 0"))
+        cmds.append(command % ('layer-gadgets:' + str(w['_pos']) + ":percent-x 0"))
+    return cmds
+
+def execute(cmds):
+    for c in cmds:
+        os.system(c)
+
 if __name__ == "__main__":
-    plstr = plistToString(sys.argv[1])
+    pathToPList = sys.argv[1]
+    plstr = plistToString(pathToPList)
     plist = parser.parsePlist(plstr)
     weathers = [i for i in plist['layer-gadgets'] if i['path'] == '/Library/Widgets/Weather.wdgt/']
-    pprint.pprint(weathers)
-
-    
+    setIndices(plist)
+    # pprint.pprint(weathers)
+    align(weathers)
+    # pprint.pprint(weathers)
+    cmds = getCommands(weathers, pathToPList)
+    execute(cmds)
 
     
